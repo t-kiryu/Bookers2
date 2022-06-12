a7f22ef0c99ee396995837ee0b5f8048785121f9
t-kiryu <104680945+t-kiryu@users.noreply.github.com>
2022-06-12 22:54:38 +0000

    [Start]Bookers2

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  root to: 'homes#top'
+
+  get 'homes/about' => 'homes#about', as: 'about'
+
+  resources :books, only: [:new, :index, :show, :edit, :update, :delete]
+  resources :users, only: [:index, :show, :edit, :update]
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
+
