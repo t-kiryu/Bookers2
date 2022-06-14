a75a2ec6e7710906f6314027776dcafca188bb19
t-kiryu <104680945+t-kiryu@users.noreply.github.com>
2022-06-14 03:58:46 +0000

    [Add]Bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
 
-  get 'homes/about' => 'homes#about', as: 'about'
+  get 'home/about' => 'home#about', as: 'about'
 
   resources :books, only: [:new, :index, :show, :edit, :update, :delete]
   resources :users, only: [:index, :show, :edit, :update]
