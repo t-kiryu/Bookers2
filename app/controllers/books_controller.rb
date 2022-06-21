8e6bd292a753051eac57d64d5bdbf7c26e9f05c3
t-kiryu <104680945+t-kiryu@users.noreply.github.com>
2022-06-21 01:15:33 +0000

    [Add]Bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,29 +1,39 @@
 class BooksController < ApplicationController
-  def new
+  def index
     @book = Book.new
+    @books = Book.all
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to book_path
-  end
-
-  def index
-    @books = Book.all
+    redirect_to book_path(@book.id)
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title, :opinion,)
+    params.require(:book).permit(:title, :body)
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   get 'home/about' => 'home#about', as: 'about'
 
-  resources :books, only: [:new, :create, :index, :show, :edit, :update, :delete]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   resources :users, only: [:index, :show, :edit, :update]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
